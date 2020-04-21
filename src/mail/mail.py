bcfb4e54a3fabbce215247955d6acfc17b17f7a5
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2020-04-21 21:17:30 +1000

    Added news to html

--- src/mail/mail.py
+++ src/mail/mail.py
@@ -3,7 +3,7 @@
 # @Author: AnthonyKenny98
 # @Date:   2020-04-16 12:13:34
 # @Last Modified by:   AnthonyKenny98
-# @Last Modified time: 2020-04-21 14:56:26
+# @Last Modified time: 2020-04-21 16:28:37
 
 import smtplib
 import ssl
